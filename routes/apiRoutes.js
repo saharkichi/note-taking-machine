e82988024a2a9024819ecc6eb5faea18444f7265
saharkichi <saharkichi@gmail.com>
2022-07-05 15:23:44 -0400

    changes2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 //require
-const db = require("../db/db.json");
+let db = require("../db/db.json");
 const fs = require("fs");
 // const express = require("express");
 const app = require("express").Router();
